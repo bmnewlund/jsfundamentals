2b8bd50d4b43385d8eea0f4c44d5b6a5ddbb4c76
Brett Newlund <bmnewlund@gmail.com>
2017-08-16 14:09:42 -0400

    Added variables

---
+++ 0.02_variables.js
@@ -0,0 +1,69 @@
+// Variables
+
+var name;
+// this is declaring the varible
+
+name = "Brett";
+
+
+console.log(typeof(name))
+
+console.log(name);
+
+
+var lastName = "Smith";
+
+console.log(name.length);
+
+// Challenge: Create two variables, "firstName", and "lastName" and concat them together 
+// in a third variable named "fullName" and print fullName to the console.
+
+console.log(lastName);
+
+
+var firstName = "Brett";
+
+lastName = "Newlund";
+
+var fullName = (firstName + " " + lastName);
+
+console.log(fullName);
+
+// Challenge: 
+
+var loanAmount = 25000;
+var hotDogBuns = 6;
+var train = 60;
+
+var myNumber = 0;
+var myNumber1 = myNumber + 10;
+var myNumber2 = myNumber1 - 1;
+var myNumber3 = myNumber2 * 9;
+var myNumber4 = myNumber3 / 7;
+
+console.log(myNumber1);
+console.log(myNumber2);
+console.log(myNumber3);
+console.log(myNumber4);
+
+var currentAge = 28;
+var maxAge = 100;
+var perDay = 3;
+var daysPerYear = 365;
+
+console.log("You will need " + ((maxAge - currentAge)) * perDay) + " " + "sandwiches to last you until the ripe old age of 100.");
+
+
+
+// Variable data types
+
+
+
+
+
+
+
+
+
+
+// Variable scopes
