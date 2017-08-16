8fcb46e75e742b28a77fefda13a21f11edcbb52a
Brett Newlund <bmnewlund@gmail.com>
2017-08-16 13:09:23 -0400

    Added data types

--- 0.01_dataTypes.js
+++ 0.01_dataTypes.js
@@ -1,1 +1,68 @@
-0.01_dataTypes.js
+// Data Types
+
+// Stings
+// Strings are a collection of characters
+
+
+"What a Wonderful world";
+'Hello you guys';
+"it's a wonderful day";
+
+console.log('Thomas said "I think grass is green!"'.toUpperCase());
+
+console.log('Could\'t things get any better'.length); 
+// the backslash in the above comment takes out the ' from the sentence
+
+
+
+// Challenge: Create a string that has your name and use the toLowerCase function to make sure eace character is lower case
+
+console.log("Brett Newlund".toLowerCase())
+
+console.log("Brett Newlund"[4].toUpperCase())
+
+console.log("brett Newlund"[0].toUpperCase() + "Brett"[1] + "Brett"[2]);
+
+// Challenge: uppercase the 'm' in Home, then back together using string concatenation.
+
+console.log("Home"[0] + "Home" [1] + "Home"[2].toUpperCase() + "Home" [3])
+
+console.log(typeof("Amber"))
+// type of will inform what "data type" is there
+
+
+
+// Numbers
+
+console.log(typeof(2))
+
+console.log(2 + 5)
+console.log(2 - 5)
+console.log(2 / 5)
+console.log(10 % 3)
+console.log(2 * 5)
+
+// BooLeans
+
+console.log()
+
+/* Conditional Operators: 
+
+
+< : less than
+> : greater than
+<= : less than or equal to
+>= : greater than or equal to
+== : equal to
+=== : strictly equal to
+!  : not 
+!=  : no equal to
+*/
+
+console.log(4 == 5);
+console.log("Test".length > "ego".length);
+console.log(1 == "1");
+console.log(1 === "1");
+
+
+
