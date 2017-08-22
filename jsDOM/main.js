038fb710a4c31767c61a8b843c9ad05f8716df04
Brett Newlund <bmnewlund@gmail.com>
2017-08-22 16:06:17 -0400

    Added jsDOM

---
+++ jsDOM/main.js
@@ -0,0 +1,48 @@
+let input = document.getElementById("input");
+let output = document.getElementById("output");
+let ulist = document.getElementById("friendsList");
+// console.log(input.value);
+
+let newName = "";
+
+function capitalize(){
+	for ( let l in input.value){
+		if (l == 0){
+			newName = input.value[l].toUpperCase();
+		} else {
+			newName += input.value[l].toLowerCase();
+		}
+	}
+	output.innerHTML = "Capitalized Name: " + newName;
+	input.value = '';
+	addItem();
+}
+
+document.onkeypress = function (e){
+	if(e.keyCode === 13){
+		capitalize();
+	}
+}
+
+function addItem(){
+	let li = document.createElement('li');
+	let text = li.innerHTML = newName;
+	ulist.appendChild(li);
+}
+
+// create element will build a new one of your chosing.
+// let test = docuent.createElement('li');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
